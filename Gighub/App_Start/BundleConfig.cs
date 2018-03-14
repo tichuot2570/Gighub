62465d1e7d4d41b84682e50597695c7c54f2af72
nam <tichuot2570@gmail.com>
2018-03-14 11:39:16 +0200

    refactor code and improve UI

--- Gighub/App_Start/BundleConfig.cs
+++ Gighub/App_Start/BundleConfig.cs
@@ -8,9 +8,13 @@ namespace Gighub
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/underscore-min.js"
+                "~/Scripts/underscore-min.js",
+                "~/Scripts/moment.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/bootbox.min.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -20,13 +24,6 @@ namespace Gighub
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootbox.min.js"
-                      
-                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
